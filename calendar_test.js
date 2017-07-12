e97ceb6e0364acde4a09d5b66124e9c733c3a029
Col Harris <col.harris@go-jek.com>
2017-07-12 22:21:14 +0800

    Added the ability to find an event with a room (a.k.a attendee)

--- calendar_test.js
+++ calendar_test.js
@@ -4,7 +4,18 @@ var moment = require('moment')
 var title = "Maker Night!"
 var startTime = moment().hours(20).minutes(0).seconds(0).milliseconds(0)
 var endTime = moment().hours(21).minutes(0).seconds(0).milliseconds(0)
+var room = "charris@thoughtworks.com"
 
-CalendarUtils.findEvent(title, startTime, endTime, (event) => {
-  console.log(`Found a matching event!`)
+CalendarUtils.findEvent(title, startTime, endTime, room, (event) => {
+  if (event) {
+    console.log(`Found a matching event!`)
+    console.log("Title:", event.summary)
+    console.log("Starting at:", event.start.dateTime)
+    console.log("Ending at:", event.end.dateTime)
+    console.log("Attendees:", event.attendees.map((attendee) => {
+      return attendee.email
+    }))
+  } else {
+    console.log(`Epic FAIL!`)
+  }
 })
